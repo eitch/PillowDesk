1d4c2549b5cb46dcfe438238406c4eee0e7d009c
Robert von Burg <eitch@eitchnet.ch>
2026-06-05 08:56:54 +0200

    [Major] Use environment variable for agent initialization fallback
    Refactored `StartupListener` to first attempt agent initialization using an environment variable before defaulting to the bootstrap file. Simplifies configuration management and enhances flexibility by supporting environment-specific setups.

--- pillow-desk-web/src/main/java/ch/eitchnet/pillowdesk/web/StartupListener.java
+++ pillow-desk-web/src/main/java/ch/eitchnet/pillowdesk/web/StartupListener.java
@@ -11,56 +11,63 @@ import li.strolch.utils.helper.StringHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.InputStream;
+import java.util.Optional;
+
+import static li.strolch.agent.api.StrolchBootstrapper.ENV_STROLCH_ENVIRONMENT;
 
 @WebListener
 public class StartupListener implements ServletContextListener {
 
-    private static final Logger logger = LoggerFactory.getLogger(StartupListener.class);
-    public static final String APP_NAME = "PillowDesk";
+	private static final Logger logger = LoggerFactory.getLogger(StartupListener.class);
+	public static final String APP_NAME = "PillowDesk";
 
-    private StrolchAgent agent;
+	private StrolchAgent agent;
 
-    @Override
-    public void contextInitialized(ServletContextEvent sce) {
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
 
-        logger.info("Starting " + APP_NAME + "...");
-        String realPath = sce.getServletContext().getRealPath("/");
-        logger.info("Real path is {}", realPath);
-        long start = System.currentTimeMillis();
-        try {
-            String bootstrapFileName = "/" + StrolchBootstrapper.FILE_BOOTSTRAP;
-            InputStream bootstrapFile = getClass().getResourceAsStream(bootstrapFileName);
-            StrolchBootstrapper bootstrapper = new StrolchBootstrapper(StartupListener.class);
-            this.agent = bootstrapper.setupByBootstrapFile(StartupListener.class, bootstrapFile);
-            this.agent.initialize();
-            this.agent.start();
-            RestfulStrolchComponent.getInstance().initialize(sce.getServletContext());
-        } catch (Throwable e) {
-            logger.error("Failed to start " + APP_NAME + " due to: {}", e.getMessage(), e);
-            throw e;
-        }
+		logger.info("Starting " + APP_NAME + "...");
 
-        long took = System.currentTimeMillis() - start;
-        logger.info("Started " + APP_NAME + " in {}", StringHelper.formatMillisecondsDuration(took));
-    }
+		String realPath = sce.getServletContext().getRealPath("/");
+		logger.info("Real path is {}", realPath);
+		long start = System.currentTimeMillis();
+		try {
 
-    @Override
-    public void contextDestroyed(ServletContextEvent sce) {
-        LoggingLoader.reloadLoggingConfiguration();
+			StrolchBootstrapper bootstrapper = new StrolchBootstrapper(StartupListener.class);
+			Optional<StrolchAgent> agentO = bootstrapper.trySetupByEnvironment(StartupListener.class);
+			this.agent = agentO.orElseGet(() -> {
+				logger.info("Environment var " + ENV_STROLCH_ENVIRONMENT + " not set, using bootstrap file...");
+				return bootstrapper.setupByBootstrapFile(StartupListener.class);
+			});
 
-        if (this.agent != null) {
-            logger.info("Destroying " + APP_NAME + "...");
-            try {
-                this.agent.stop();
-                this.agent.destroy();
-            } catch (Throwable e) {
-                logger.error("Failed to stop " + APP_NAME + " due to: {}", e.getMessage(), e);
-                throw e;
-            } finally {
-                logger.info("Destroyed " + APP_NAME);
-                LoggingLoader.reset();
-            }
-        }
-    }
+			this.agent.initialize();
+			this.agent.start();
+			RestfulStrolchComponent.getInstance().initialize(sce.getServletContext());
+		} catch (Throwable e) {
+			logger.error("Failed to start " + APP_NAME + " due to: {}", e.getMessage(), e);
+			throw e;
+		}
+
+		long took = System.currentTimeMillis() - start;
+		logger.info("Started " + APP_NAME + " in {}", StringHelper.formatMillisecondsDuration(took));
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		LoggingLoader.reloadLoggingConfiguration();
+
+		if (this.agent != null) {
+			logger.info("Destroying " + APP_NAME + "...");
+			try {
+				this.agent.stop();
+				this.agent.destroy();
+			} catch (Throwable e) {
+				logger.error("Failed to stop " + APP_NAME + " due to: {}", e.getMessage(), e);
+				throw e;
+			} finally {
+				logger.info("Destroyed " + APP_NAME);
+				LoggingLoader.reset();
+			}
+		}
+	}
 }
